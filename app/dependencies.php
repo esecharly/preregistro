3af8593b31fa57a82d229b6cbfc50c04954aa6d7
esecharly <62493210+esecharly@users.noreply.github.com>
2021-04-22 14:20:56 -0500

    update manager_registro dependencies

--- app/dependencies.php
+++ app/dependencies.php
@@ -2,5 +2,5 @@
 
 <script src="<?=SERVIDOR?>raw/js/jquery-3.6.0.js"></script>
 <script src="<?=SERVIDOR?>raw/js/bootstrap.js"></script>
-<script src="<?=SERVIDOR?>raw/js/swal.js"></script>
+<!-- <script src="<?=SERVIDOR?>raw/js/swal.js"></script> -->
 <script src="<?=SERVIDOR?>raw/js/sw.js"></script>
--- manager/manager_registro.js
+++ manager/manager_registro.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
             swal('Upps', 'Ingresa tu nombre por favor', 'warning');
             return false;
         }
+        if ($('#registro_paterno').val() =="") {
+            swal('Upps', 'Ingresa tu nombre por favor', 'warning');
+            return false;
+        }
+        if ($('#registro_materno').val() =="") {
+            swal('Upps', 'Ingresa tu nombre por favor', 'warning');
+            return false;
+        }
+        if ($('#registro_nombre').val() =="") {
+            swal('Upps', 'Ingresa tu nombre por favor', 'warning');
+            return false;
+        }
     };
 
     $('#btn_registro_usuario').click(function() {
